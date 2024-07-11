ace799d4910b18dac7bd1a935ba5caba1c128e01
yair lavie <yair.lavie2008@gmail.com>
2024-07-11 23:51:51 +0300

    fixes.3
    fixing some more things

--- Off-Season Robot Code/src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
+++ Off-Season Robot Code/src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -2,8 +2,8 @@ package frc.robot.subsystems.Shooter;
 
 public interface ShooterConstants {
     // Shooter Motor IDs
-    int UPMOTOR_ID = 6; //TODO: find id
-    int DOWNMOTOR_ID = 7; //TODO: find id
+    int UP_MOTOR_ID = 6; //TODO: find id
+    int DOWN_MOTOR_ID = 7; //TODO: find id
 
     double MINIMUM_ERROR = 5;
 
