2ff2ff548ae2157d29bf9501d1a3ce431bc4a5ad
Avizi <ziv.avivi.28.11@gmail.com>
2024-07-16 23:39:37 +0300

    something
    abc

---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
@@ -0,0 +1,13 @@
+// melon i have no idea wat im doing i tink i removed somthin idk
+package frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
