70204b9f79ce88bad3b469d19ef2edc3b673ec18
yair lavie <yair.lavie2008@gmail.com>
2024-07-16 23:13:59 +0300

    configs, mm and interpolation
    adding the content of configs adding mm and its values and the values of the interpolation

--- src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -7,4 +7,37 @@ public interface ShooterConstants {
 
     double MINIMUM_ERROR = 5;
 
+    // Motion Magic Values
+    int MOTION_MAGIC_CRUISE_VELOCITY = 80;
+    int MOTION_MAGIC_ACCELERATION = 160;
+    int MOTION_MAGIC_JERK = 1600;
+
+    double PEAK_FORWARD_VOLTAGE = 11.5;
+    double PEAK_REVERSE_VOLTAGE = -11.5;
+
+    double GEAR_RATIO = 0.5;
+
+    int MAX_ERROR = 2;
+
+    // PID values for up motor
+    double UP_KP = 0.056;
+    double UP_KD = 0.0;
+    double UP_KS = 0.16;
+    double UP_KV = 0.056;
+    double UP_KA = 0.0;
+
+   // PID values for down motor
+    double DOWN_KP = 0.022;
+    double DOWN_KD = 0.0;
+    double DOWN_KS = 0.2998046875;
+    double DOWN_KV = 0.0583;
+    double DOWN_KA = 0.079175;
+
+   // Interpolatio
+    double SHOOT_BASE_SPEED = 50;
+    double SHOOT_STAGE_SPEED = 70;
+    double SHOOT_SPEED = 70;
+    double AMP_SPEED = 23;
+
+
 }
--- src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
@@ -4,7 +4,14 @@
 
 package frc.robot.subsystems.Shooter;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,6 +21,7 @@ public class ShooterSubsystem extends SubsystemBase implements ShooterConstants 
   /** Creates a new ShooterSubsystem. */
   private TalonFX up_Motor;
   private TalonFX down_Motor;
+  private MotionMagicVelocityVoltage mm = new MotionMagicVelocityVoltage(0,MOTION_MAGIC_ACCELERATION,true,0.0,0,false,false,false);
   private double targetSpeed = 0;
 
   // using a singleton
@@ -34,12 +42,10 @@ public class ShooterSubsystem extends SubsystemBase implements ShooterConstants 
   }
 
   /** a command for setting a speed to the motors */
-  public Command setShooterSpeed(double speed) {
+  public Command setShooterSpeed(double speed){
     targetSpeed = speed;
-    return runOnce(() -> {
-      up_Motor.set(speed);
-      down_Motor.set(speed);
-    });
+    return runOnce(() -> {up_Motor.setControl(mm.withVelocity(speed));
+    down_Motor.setControl(mm.withVelocity(speed));});
   }
 
   /** a command for stopping the motors */
@@ -61,6 +67,73 @@ public class ShooterSubsystem extends SubsystemBase implements ShooterConstants 
   public void periodic() {
     // This method will be called once per scheduler run
   }
-  private void configs(){}
+  private void configs(){
+    // declaring Configs
+        TalonFXConfiguration upConfigs = new TalonFXConfiguration();
+        TalonFXConfiguration downConfigs = new TalonFXConfiguration();
+        MotionMagicConfigs shooterMM = new MotionMagicConfigs();
+
+        // giving motion magic values
+        shooterMM.MotionMagicCruiseVelocity =  MOTION_MAGIC_CRUISE_VELOCITY;
+        shooterMM.MotionMagicAcceleration =  MOTION_MAGIC_ACCELERATION;
+        shooterMM.MotionMagicJerk =  MOTION_MAGIC_JERK;
+        upConfigs.MotionMagic = shooterMM;
+        downConfigs.MotionMagic = shooterMM;
+
+        // giving PID values
+        upConfigs.Slot0.kP =  UP_KP;
+        upConfigs.Slot0.kD =  UP_KD;
+        upConfigs.Slot0.kS =  UP_KS;
+        upConfigs.Slot0.kV =  UP_KV;
+        upConfigs.Slot0.kA =  UP_KA;
+
+        downConfigs.Slot0.kP =  DOWN_KP;
+        downConfigs.Slot0.kD =  DOWN_KD;
+        downConfigs.Slot0.kS =  DOWN_KS;
+        downConfigs.Slot0.kV =  DOWN_KV;
+        downConfigs.Slot0.kA =  DOWN_KA;
+
+        // max voltage for m_shooterMotor
+        upConfigs.Voltage.PeakForwardVoltage =  PEAK_FORWARD_VOLTAGE;
+        upConfigs.Voltage.PeakReverseVoltage =  PEAK_REVERSE_VOLTAGE;
+
+        downConfigs.Voltage.PeakForwardVoltage =  PEAK_FORWARD_VOLTAGE;
+        downConfigs.Voltage.PeakReverseVoltage =  PEAK_REVERSE_VOLTAGE;
+
+        upConfigs.Feedback.SensorToMechanismRatio =  GEAR_RATIO;
+        downConfigs.Feedback.SensorToMechanismRatio =  GEAR_RATIO;
+
+        upConfigs.CurrentLimits.SupplyCurrentLimit = 40;
+        upConfigs.CurrentLimits.SupplyCurrentThreshold = 50;
+        upConfigs.CurrentLimits.SupplyTimeThreshold = 0.1;
+        upConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+        downConfigs.CurrentLimits.SupplyCurrentLimit = 40;
+        downConfigs.CurrentLimits.SupplyCurrentThreshold = 50;
+        downConfigs.CurrentLimits.SupplyTimeThreshold = 0.1;
+        downConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+
+        up_Motor.setNeutralMode(NeutralModeValue.Coast);
+        down_Motor.setNeutralMode(NeutralModeValue.Coast);
+
+        upConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        downConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+        /* Speed up signals for better characterization data */
+        BaseStatusSignal.setUpdateFrequencyForAll(1000, up_Motor.getVelocity());
+        BaseStatusSignal.setUpdateFrequencyForAll(1000, down_Motor.getVelocity());
+
+        up_Motor.optimizeBusUtilization();
+        down_Motor.optimizeBusUtilization();
+
+        // Checking if up_Motor apply configs
+        StatusCode status = StatusCode.StatusCodeNotInitialized;
+        for (int i = 0; i < 5; ++i) {
+            status = up_Motor.getConfigurator().apply(upConfigs);
+            if (status.isOK())
+                break;
+        }
+                
+  }
 
 }
