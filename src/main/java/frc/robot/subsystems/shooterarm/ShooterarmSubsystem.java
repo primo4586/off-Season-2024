b5bafb3c07a5a734ec9a5a135ffc4ea78494f2e9
amit levy <amitlevy104@gmail.com>
2024-08-25 13:21:46 +0300

    added constants and a little more
    get back to work levy

--- src/main/java/frc/robot/subsystems/shooterarm/ShooterarmSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooterarm/ShooterarmSubsystem.java
@@ -2,19 +2,59 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.shooterarm;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Shooter.ShooterConstants;
-
-public class ShooterarmSubsystem extends SubsystemBase implements ShooterConstants{
-  /** Creates a new ShooterarmSubsystem. */
-  private TalonFX Motor;
+public class ShooterarmSubsystem extands SubsystemBase implements ShooterarmSubsystem{
+  private TalonFX m_shooterArmMotor;
+  private DigitalInput m_limitSwitch;
   
-  public ShooterarmSubsystem() {}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+
+private void configs(){
+    TalonFXConfiguration configuration = new TalonFXConfiguration();
+    MotionMagicConfigs mm =new MotionMagicConfigs();
+
+    //peeks
+    configuration.CurrentLimits.SupplyCurrentLimitEnable=true;
+    configuration.CurrentLimits.SupplyCurrentLimit= PEAK_CURRENT;
+
+    mm.MotionMagicCruiseVelocity=MM_CRUISE;
+    mm.MotionMagicAcceleration=MM_ACCELERATION;
+    mm.MotionMagicJerk=MM_JERK;
+
+    //slot
+    configuration.Slot0.kP=KP;
+    configuration.Slot0.kD=KD;
+    configuration.Slot0.kV=KV;
+    configuration.Slot0.kS=KS;
+    configuration.Slot0.kA=KA;
+
+    // forward  
+    configuration.SoftwareLimitSwitch.ForwardSoftLimitEnable=true;
+    configuration.SoftwareLimitSwitch.ReverseSoftLimitEnable=true;
+    configuration.SoftwareLimitSwitch.ForwardSoftLimitThreshold=FOWORD_LIMIT;
+    configuration.SoftwareLimitSwitch.ReverseSoftLimitThreshold=BACKWARD_LIMIT;
+
+    configuration.HardwareLimitSwitch.ReverseLimitAutosetPositionEnable=true;
+    configuration.HardwareLimitSwitch.ReverseLimitAutosetPositionValue=RESET_POSITION;
+
+    //setings
+    configuration.Feedback.SensorToMechanismRatio=TICKS_PER_DEGREE;
+    configuration.MotorOutput.NeutralMode=NEUTRAL_MODE;
+    configuration.MotorOutput.Inverted=INVERTED;
+
+    StatusCode statusCode = StatusCode.StatusCodeNotInitialized;
+    for(int i=0; i<5;i++){
+      statusCode = m_shooterArmMotor.getConfigurator().apply(configuration);
+      if(statusCode.isOK())
+        break;
+
+
+
+    }
+    if(!statusCode.isOK())
+    System.out.println("shooter Arm could not apply config, error cod"+ statusCode.toString());
+
+
   }
 }
+
+  
