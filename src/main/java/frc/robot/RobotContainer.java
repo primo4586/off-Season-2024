a0f1eefe2f14ccbee35574669a3614a3d0331ea8
Primo4586 <primo4586@gmail.com>
2024-10-08 12:22:29 +0300

    UNSECSEFLE SWERVE

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,87 +1,61 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.CommandGroupFactory;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.Shooter.ShooterSubsystem;
-import frc.robot.subsystems.ShooterArmFolder.ShooterArmSubsystem;
-import frc.robot.subsystems.intake.IntakeSubsystem;
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 
-import com.ctre.phoenix6.SignalLogger;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
+import frc.robot.subsystems.swerve.TunerConstants;
 
 public class RobotContainer {
+  private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
+  private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-    new CommandXboxController(0);
-  private final CommandXboxController m_operatorController =
-    new CommandXboxController(1);
-  private final CommandXboxController m_testerController =
-    new CommandXboxController(2);
-  private final CommandXboxController m_SYSidController = new CommandXboxController(3);
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
+  private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
 
-  private final IntakeSubsystem intake = IntakeSubsystem.getInstance();
-  private  final ShooterSubsystem shooter = ShooterSubsystem.getInstance();
-  private final ShooterArmSubsystem shooterArm = ShooterArmSubsystem.getInstance();
-      
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+      .withDeadband(MaxSpeed * 0.02).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // I want field-centric
+                                                               // driving in open loop
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+  private final Telemetry logger = new Telemetry(MaxSpeed);
+
+  private void configureBindings() {
+    drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+        drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
+                                                                                           // negative Y (forward)
+            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+        ));
+
+    joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    joystick.b().whileTrue(drivetrain
+        .applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+
+    // reset the field-centric heading on left bumper press
+    joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+
+    if (Utils.isSimulation()) {
+      drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
+    }
+    drivetrain.registerTelemetry(logger::telemeterize);
+  }
+
   public RobotContainer() {
     configureBindings();
-  
-
-    
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   * 
-   */
-  private void configureBindings() {
-    // basic test commands
-    m_testerController.a().onTrue(intake.coolectUntilNoteCommand());
-    m_testerController.y().onTrue(intake.setCurrentCommand());
-    //m_testerController.x().whileTrue(shooter.setShooterSpeed(0)); THERE IS NO PID YET DO NOT TRY 
-    m_testerController.start().onTrue(shooterArm.prepareHomeCommand()); //TODO: test if it works 
-    m_testerController.b().whileTrue(shooterArm.setSpeed(0.2));
-
-    
-
-    m_testerController.rightTrigger().onTrue(CommandGroupFactory.yeet());
-    m_testerController.rightBumper().onTrue(CommandGroupFactory.shootFromBase());
-    
-    // SYSid
-    /* 
-    m_SYSidController.start().onTrue(Commands.runOnce(SignalLogger::start));
-    m_SYSidController.back().onTrue(Commands.runOnce(SignalLogger::stop));
-
-    m_SYSidController.a().whileTrue(shooterArm.sysIdDynamic((SysIdRoutine.Direction.kForward)));
-    m_SYSidController.b().whileTrue(shooterArm.sysIdDynamic((SysIdRoutine.Direction.kReverse)));
-    m_SYSidController.x().whileTrue(shooterArm.sysIdQuasistatic((SysIdRoutine.Direction.kForward)));
-    m_SYSidController.y().whileTrue(shooterArm.sysIdDynamic((SysIdRoutine.Direction.kReverse)));
-    */
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
   }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
 }
