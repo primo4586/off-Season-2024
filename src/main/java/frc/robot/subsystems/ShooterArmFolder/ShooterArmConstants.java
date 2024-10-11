d0c27f9232b667ab6ad63a022d34c973783cba06
eilonhuppert <eilonhuppert@gmail.com>
2024-10-11 10:10:08 +0300

    fixed intake push back

--- src/main/java/frc/robot/subsystems/ShooterArmFolder/ShooterArmConstants.java
+++ src/main/java/frc/robot/subsystems/ShooterArmFolder/ShooterArmConstants.java
@@ -36,7 +36,7 @@ public interface ShooterArmConstants {
     true);
 
     // Condition Constants
-    double MINIMUM_ERROR = 0.5;
+    double MINIMUM_ERROR = 1;
     double RESET_SPEED = -0.2;
 
     double BASE_ANGLE = 35.5;
