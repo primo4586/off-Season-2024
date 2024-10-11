22693415c18dafa08d7d4e34aaebc38fcad10fe3
Primo4586 <primo4586@gmail.com>
2024-10-11 10:08:26 +0300

    amp is ready

--- src/main/java/frc/robot/subsystems/AMP/AmpSubsystem.java
+++ src/main/java/frc/robot/subsystems/AMP/AmpSubsystem.java
@@ -2,20 +2,13 @@ package frc.robot.subsystems.AMP;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkLimitSwitch;
 import com.revrobotics.SparkPIDController;
-import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class AmpSubsystem extends SubsystemBase implements AmpConstants{
@@ -39,27 +32,22 @@ public class AmpSubsystem extends SubsystemBase implements AmpConstants{
         _armMotor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
         encoder = new TalonSRX(ENCODER_ID);
         configs();
+        resetToAbsultPosition();
     }
 
     // Method to move the arm to a specified position using Motion Magic
-    public void moveToPosition(double targetPosition) {
+    public Command moveToPosition(double targetPosition) {
+        
         // Clamp the target position to ensure it's within the limits
-        targetPosition = Math.max(MIN_POSITION, Math.min(MAX_POSITION, targetPosition));
+        double clampedTargetPosition = Math.max(MIN_POSITION, Math.min(MAX_POSITION, targetPosition));
 
-        // Calculate the feedforward output based on velocity
-        double feedforwardOutput = feedforward.calculate(encoderTalon.getSelectedSensorVelocity());
-
-        // Use Motion Magic to move to the target position
+        return this.runOnce(()->
+            _armMotor.getPIDController().setReference(clampedTargetPosition, ControlType.kPosition));
     }
 
     // Method to return the current position of the arm
     public double getPosition() {
-        return encoder.getSelectedSensorPosition();
-    }
-
-    // Method to return the current velocity of the arm
-    public double getVelocity() {
-        return encoder.getSelectedSensorVelocity();
+        return _armMotor.getEncoder().getPosition();
     }
 
     @Override
@@ -69,7 +57,7 @@ public class AmpSubsystem extends SubsystemBase implements AmpConstants{
     }
 
     private void configs(){
-        SparkPIDController configuration = _armMotor.getPIDController();
+        SparkPIDController sparkPIDController = _armMotor.getPIDController();
 
         _armMotor.setInverted(INVERTED);
         _armMotor.setSmartCurrentLimit(CURRENT_LIMIT);
@@ -77,13 +65,15 @@ public class AmpSubsystem extends SubsystemBase implements AmpConstants{
 
         //PID
         
-        configuration.setP(kP);
-        configuration.setD(kD);
+        sparkPIDController.setP(kP);
+        sparkPIDController.setD(kD);
 
         // forward and backward limits 
         
         _armMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, MAX_POSITION);
         _armMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, MIN_POSITION);
+
+        _armMotor.getEncoder().setPositionConversionFactor(1/147);
 
         encoder.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
     }
